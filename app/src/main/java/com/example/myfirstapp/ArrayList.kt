8270f018019238204523505621a94f619d7572f6
Nabeel Ahmad <nabeel.oraxtech@gmail.com>
2023-12-05 19:23:41 +0500

    Updated in arraylist

--- app/src/main/java/com/example/myfirstapp/ArrayList.kt
+++ app/src/main/java/com/example/myfirstapp/ArrayList.kt
@@ -1,5 +1,7 @@
 package com.example.myfirstapp
 
+import android.util.Log
+
 fun main(){
     val newArrayList= arrayListOf<Double>(1.2,3.6,6.8,4.7,4.6,9.6)
     var total=0.0;
@@ -8,4 +10,11 @@ fun main(){
     }
     var newtotal=total/5
     print("Average of the numbers is $newtotal")
+
+    //Lambda Expressions
+
+    Log.d("msg","output")
+     val sum: (Int,Int) -> Int = {a, b -> a+b}
+
+            println(sum(4,7));
 }
